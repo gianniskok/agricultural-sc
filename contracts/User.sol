24e52dcd7522a57595ae80c54972194f79d27dfc
gianniskok <giannis.kokkoros@hotmail.com>
2021-12-10 08:00:40 -0500

    user

--- contracts/User.sol
+++ contracts/User.sol
@@ -4,10 +4,19 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract User {
-    address payable private owner;
+    address  payable private immutable owner;
 
     constructor() {
         owner = payable(msg.sender);
+    }
+
+    enum UserRole {
+        Farmer,
+        Transporter,
+        Storage,
+        Inspector,
+        Packager,
+        Retailer
     }
 
    
