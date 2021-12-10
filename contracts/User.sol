38fcf615394fd47bfb77a9327750b08e1f060310
gianniskok <giannis.kokkoros@hotmail.com>
2021-12-10 08:50:22 -0500

    owner and user changes

--- contracts/User.sol
+++ contracts/User.sol
@@ -2,13 +2,11 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
-
-contract User {
-    address  payable private immutable owner;
-
-    constructor() {
-        owner = payable(msg.sender);
-    }
+import "./Owner.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+contract User is Owner {
+    using Counters for Counters.Counter;
+    Counters.Counter private _userId;
 
     enum UserRole {
         Farmer,
@@ -16,8 +14,67 @@ contract User {
         Storage,
         Inspector,
         Packager,
-        Retailer
+        Retailer,
+        Admin
     }
 
+    struct UserDetails {
+        string userName;
+        address userAddress;
+        uint256 userId;
+        bool isActive;
+        UserRole role;
+        uint256 registrationDate;        
+    }
+
+    UserDetails[] public UserStruct;
+    mapping(address => UserDetails) public addressToUser;
+
+    modifier onlyAdmin {
+        require(addressToUser[msg.sender].role == UserRole.Admin, "#OACA"); //Only admin can access this
+        _;
+    }
+
+    event adminRegistered(address indexed userAddress);
+    event userRegistered(address indexed userAddress);
+    event userDeactivated(address indexed userAddress);
+
+    function registerAdmin(string memory _userName, address _userAddress) external onlyOwner {
+        require(addressToUser[_userAddress].userAddress != _userAddress, "UAE"); //User already exists
+        UserDetails memory newUser;
+        _userId.increment();
+        newUser.userName = _userName;
+        newUser.userAddress = _userAddress;
+        newUser.userId = _userId.current();
+        newUser.isActive = true;
+        newUser.role = UserRole.Admin;
+        newUser.registrationDate = block.timestamp;
+        addressToUser[_userAddress] = newUser;
+        UserStruct.push(newUser);
+        emit adminRegistered(_userAddress);
+    }
+
+    function registerUser(string memory _userName, address _userAddress, uint256 _userRole) external onlyAdmin {
+        require(addressToUser[_userAddress].userAddress != _userAddress, "UAE"); //User already exists
+        UserDetails memory newUser;
+        _userId.increment();
+        newUser.userName = _userName;
+        newUser.userAddress = _userAddress;
+        newUser.userId = _userId.current();
+        newUser.isActive = true;
+        newUser.role = UserRole(_userRole);
+        newUser.registrationDate = block.timestamp;
+        addressToUser[_userAddress] = newUser;
+        UserStruct.push(newUser);
+        emit userRegistered(_userAddress);
+    }
+
+    function deActivateUser(address _userAddress) external onlyAdmin {
+        require(addressToUser[_userAddress].userAddress == _userAddress, "UDE"); //User doesnt exist
+        require(addressToUser[_userAddress].isActive == true, "UAD"); //User Already Deactivated
+        addressToUser[_userAddress].isActive = false;
+        UserStruct[addressToUser[_userAddress].userId -1].isActive = false;
+        emit userDeactivated(_userAddress);
+    }
    
 }
