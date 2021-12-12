d8213720188e67317977251345c5431993df7fa0
gianniskok <giannis.kokkoros@hotmail.com>
2021-12-12 16:08:49 -0500

    .

--- contracts/Interface.sol
+++ contracts/Interface.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.0;
 
 import "./Farm.sol";
 import "./Transport.sol";
+import "./Inspect.sol";
+import "./Storage.sol";
 
-contract Interface is Farm, Transport {
+
+contract Interface is Farm, Transport, Inspect, Storage {
     constructor(){}
 }
