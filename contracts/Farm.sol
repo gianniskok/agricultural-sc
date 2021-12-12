db2d178c91354746473e29da790bacc5c4294649
gianniskok <giannis.kokkoros@hotmail.com>
2021-12-12 08:37:03 -0500

    added interface.sol

--- contracts/Farm.sol
+++ contracts/Farm.sol
@@ -6,11 +6,13 @@ import "./RawGoods.sol";
 
 contract Farm is User {
     mapping(address => address[]) farmerToRawContracts;
+    mapping(address => string) public rawGoodsAddressToType;
 
     function createRawGoods(uint256 _quantity, uint256 _price, uint256 _expDate, string memory _type, string memory _location) external onlyFarmer {
         address farmer = msg.sender;
         RawGoods newGood = new RawGoods(_quantity, _price, _expDate, farmer, _type, _location);
         farmerToRawContracts[msg.sender].push(address(newGood));
+        rawGoodsAddressToType[address(newGood)] = _type;
     }
 
     function sendGoodsForInspection(address _rawAddress) external onlyFarmer {
